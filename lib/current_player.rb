6f6308618d6b5d1f66cfeb77c1edf2624699a9e0
Ingrid Wong <yw340@georgetown.edu>
2017-12-26 18:52:00 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,10 @@
 def turn_count(board)
-  number_of_turns_played
+  number_of_turns_played = 0
 
+  board.each do |turn|
+    if turn == "X" || turn == "O"
+      number_of_turns_played ++
+    end 
+  end 
   return number_of_turns_played
 end
